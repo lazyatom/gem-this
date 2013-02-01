3701ad58237ae5b7071772f0a8083b135a742de9
James Adam <james@lazyatom.com>
2013-02-01 14:10:37 +0000

    Use Bundler when developing.

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source :rubygems
+
+gem 'shoulda'
+gem 'rake', '0.9.2'
+gemspec
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,5 @@
 require 'rubygems'
+require 'bundler/setup'
 require 'shoulda'
 require 'fileutils'
 require 'tempfile'
